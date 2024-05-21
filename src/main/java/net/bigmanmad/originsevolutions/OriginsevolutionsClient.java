a303db4762b882aa795c0f0da27ef6ea8cb7f4fb
bigmanmad <lewisgoreham@icloud.com>
2024-05-21 11:56:50 +0100

    Initial commit

---
+++ src/main/java/net/bigmanmad/originsevolutions/OriginsevolutionsClient.java
@@ -0,0 +1,10 @@
+package net.bigmanmad.originsevolutions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OriginsevolutionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/bigmanmad/originsevolutions/item/ModItems.java
@@ -0,0 +1,9 @@
+package net.bigmanmad.originsevolutions.item;
+
+import net.bigmanmad.originsevolutions.Originsevolutions;
+
+public class ModItems {
+    public static void registerModItems() {
+        Originsevolutions.LOGGER.info("Registering Mod Items for " + Originsevolutions.MOD_ID);
+    }
+}
